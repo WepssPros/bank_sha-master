5aef0527e9db84ad3f03dc4ba8cde477ca4c44be
WepssPros <reyhandwiambrta@gmail.com>
2024-05-29 13:53:59 +0700

    first Android Commit

--- lib/ui/pages/tagihan_page.dart
+++ lib/ui/pages/tagihan_page.dart
@@ -1,39 +1,93 @@
 import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/blocs/pembayaran_form/pembayaran_form_bloc.dart';
 import 'package:bank_sha/blocs/tagihan/tagihan_bloc.dart';
-
 import 'package:bank_sha/models/pembayaran_form_model.dart';
-import 'package:bank_sha/models/transfer_form_model.dart';
-import 'package:bank_sha/models/user_model.dart';
+import 'package:bank_sha/models/tagihan_model.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/pages/pembayaran_succes.dart';
-import 'package:bank_sha/ui/pages/transfer_amount_page.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
-import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:bank_sha/ui/widgets/tagihan_item.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TagihanPage extends StatefulWidget {
   const TagihanPage({Key? key}) : super(key: key);
+
   @override
   State<TagihanPage> createState() => _TagihanPageState();
 }
 
 class _TagihanPageState extends State<TagihanPage> {
+  TagihanModel? selectedTagihan;
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void _confirmAndPay(BuildContext context) {
+    if (selectedTagihan != null) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Konfirmasi Pembayaran'),
+          content: Text(
+            'Anda akan melakukan pembayaran untuk tagihan Pam ${selectedTagihan!.noPam.no_pam}. Lanjutkan?',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () {
+                final authState = context.read<AuthBloc>().state;
+                String userPin = '';
+                if (authState is AuthSuccess) {
+                  userPin = authState.data.pin!;
+                }
+
+                context.read<PembayaranFormBloc>().add(
+                      PembayaranFormPost(
+                        PembayaranFormModel(
+                          tagihan_pam_id: selectedTagihan!.id.toString(),
+                        ),
+                      ),
+                    );
+
+                Navigator.of(context).pop(); // Tutup dialog konfirmasi
+              },
+              child: const Text('Ya'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Pilih Tagihan'),
+          content: const Text('Silakan pilih tagihan yang ingin dibayar.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Tutup dialog
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Tagihan',
-        ),
+        title: const Text('Tagihan'),
       ),
       body: BlocConsumer<PembayaranFormBloc, PembayaranFormState>(
         listener: (context, state) {
@@ -63,15 +117,12 @@ class _TagihanPageState extends State<TagihanPage> {
           }
         },
         builder: (context, state) {
+          final bool isSelected;
           return ListView(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 24),
             children: [
               Container(
-                margin: const EdgeInsets.only(
-                  top: 30,
-                ),
+                margin: const EdgeInsets.only(top: 30),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -98,53 +149,16 @@ class _TagihanPageState extends State<TagihanPage> {
                                 children: state.tagihans.map((tagihan) {
                                   return GestureDetector(
                                     onTap: () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                          title: const Text(
-                                              'Konfirmasi Pembayaran'),
-                                          content: Text(
-                                            'Anda akan melakukan pembayaran untuk tagihan Pam ${tagihan.noPam.no_pam}. Lanjutkan?',
-                                          ),
-                                          actions: [
-                                            TextButton(
-                                              onPressed: () {
-                                                Navigator.of(context).pop();
-                                              },
-                                              child: const Text('Batal'),
-                                            ),
-                                            TextButton(
-                                              onPressed: () {
-                                                final authState = context
-                                                    .read<AuthBloc>()
-                                                    .state;
-                                                String userPin = '';
-                                                if (authState is AuthSuccess) {
-                                                  userPin = authState.data.pin!;
-                                                }
-
-                                                context
-                                                    .read<PembayaranFormBloc>()
-                                                    .add(
-                                                      PembayaranFormPost(
-                                                        PembayaranFormModel(
-                                                          tagihan_pam_id:
-                                                              tagihan.id
-                                                                  .toString(),
-                                                        ),
-                                                      ),
-                                                    );
-
-                                                Navigator.of(context)
-                                                    .pop(); // Tutup dialog konfirmasi
-                                              },
-                                              child: const Text('Ya'),
-                                            ),
-                                          ],
-                                        ),
-                                      );
+                                      setState(() {
+                                        selectedTagihan = tagihan;
+                                      });
                                     },
-                                    child: TagihanItem(tagihan: tagihan),
+                                    child: Container(
+                                      margin: const EdgeInsets.only(bottom: 10),
+                                      child: TagihanItem(
+                                        tagihan: tagihan,
+                                      ),
+                                    ),
                                   );
                                 }).toList(),
                               );
@@ -159,6 +173,12 @@ class _TagihanPageState extends State<TagihanPage> {
                   ],
                 ),
               ),
+              const SizedBox(height: 24),
+              CustomFilledButton(
+                title: 'Bayar Air',
+                onPressed: () => _confirmAndPay(context),
+              ),
+              const SizedBox(height: 57),
             ],
           );
         },
